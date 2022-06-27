6a80875f6f89387e56499068c5c28c8591fa6fa0
Yash Yadav <yashyadav78270@gmail.com>
2022-06-28 00:03:19 +0530

    Added base directory

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const header = () => {
   return (
     <section className="header">
-      <h1>Good Evening pipeline with build</h1>
+      <h1>Testing Invalidate</h1>
       <div className="card-wrapper">
         <div className="card">
           <img src="../Images/liked.png" alt="" />
